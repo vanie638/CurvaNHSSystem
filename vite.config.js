def686e22372f5137bd31e2fc685ca843775eee1
Your Name <jovanie.dasian@student.passerellesnumeriques.org>
2023-08-01 16:32:24 +0800

    ss

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,16 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/style.css', 'resources/css/bootstrap.min.css'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+
+    css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: `@import "resources/css/style.css";`,
+            additionalData: `@import "resources/css/bootstrap.min.css";`,
+        },
+      },
 });
