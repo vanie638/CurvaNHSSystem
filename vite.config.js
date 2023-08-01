533d503119769a19ca7baedff591794275bdbc59
Your Name <jovanie.dasian@student.passerellesnumeriques.org>
2023-08-01 16:07:48 +0800

    change

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/style.css', 'resources/css/bootstrap.min.css'],
             refresh: true,
         }),
     ],
